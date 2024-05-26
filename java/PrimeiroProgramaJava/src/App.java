4b558964d76869cb245cda93297eff36b428e3cf
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-05-26 20:31:30 -0300

    criei pasta Java

---
+++ java/PrimeiroProgramaJava/src/App.java
@@ -0,0 +1,18 @@
+class App
+{
+    public static void main (String entrada[])
+    {
+        int inteiro = 19;
+        double real = 1.78;
+        String frase = "Vini";
+        boolean VF = true;
+
+        if (VF == true)
+        {
+            System.out.println("Eu sou o " + frase + " tenho " + inteiro + " anos e tenho " + real + "m de altura");
+        }
+
+        System.exit(0);
+
+    }
+}
---
+++ java/PrimeiroProgramaJava/src/exer.java
@@ -0,0 +1,18 @@
+class exer
+{
+    public static void main (String entrada[])
+    {
+        int inteiro = 19;
+        double real = 77;
+        String nome = "Vinicius Borges";
+        String curso = "ADS";
+        char genero = 'M';
+
+        {
+           System.out.println("Meu nome: " + nome + "\nMeu curso: " + curso + "\nMinha idade: " + inteiro + "\nMeu genero: " + genero + "\nMeu peso: " + real);
+        }
+
+        System.exit(0);
+
+    }
+}
