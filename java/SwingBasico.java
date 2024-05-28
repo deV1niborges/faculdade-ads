7160fcb660c978ae96cbd5415ede9bd502302d7d
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-05-27 22:42:59 -0300

    Update SwingBasico.java

--- java/SwingBasico.java
+++ java/SwingBasico.java
@@ -11,7 +11,7 @@ class SwingBasico {
         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
 
         div = (int)n1 / (int)n2;
-        pot = Math.pow(n1, 1/n2);
+        pot = Math.pow(n1, n2);
 
         msg = msg + "quociente da divisao de " + n1 + " por " + n2 + " = " + div + "\n";
         msg = msg + "potencia de " + n1 + " por " + n2 + " = " + pot + "\n";
