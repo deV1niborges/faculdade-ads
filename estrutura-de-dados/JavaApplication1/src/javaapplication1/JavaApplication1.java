dc2afdfa81b1f7f94aced7d89f5246c94129f713
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-08-12 16:07:26 -0300

    odeio java kk

--- estrutura-de-dados/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ estrutura-de-dados/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -4,17 +4,10 @@
  */
 package javaapplication1;
 
-/**
- *
- * @author bv202
- */
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Como eu odeio Java");
     }
     
 }
